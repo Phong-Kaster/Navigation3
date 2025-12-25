b9f8f695e4838b78a4c08ca0e65304851fb1f43b
Phong Kaster <phongnt@apero.vn>
2025-12-25 15:27:17 +0700

    feature: complete build basic navigation host which Use rememberNavBackStack

--- app/src/main/java/com/example/navigationv3/ui/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/navigationv3/ui/navigation/NavigationHost.kt
@@ -1,14 +1,7 @@
 package com.example.navigationv3.ui.navigation
 
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
-import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
@@ -21,11 +14,17 @@ import com.example.navigationv3.ui.screen.splash.SplashScreen
 
 /**
  * [Display the back stack](https://developer.android.com/guide/navigation/navigation-3/basics)
+ * 1.In Navigation 3, the back stack does not actually contain content. Instead, it contains references to content,
+ * known as keys. Keys can be any type but are usually simple, serializable data classes.
+ *
+ * 2. NavEntry, which is a class containing a composable function. It represents a destination - a
+ * single piece of content that the user can navigate forward to and back from.
+ *
+ *
  */
 @Composable
-fun NavHost() {
+fun NavigationHost() {
     val TAG = "Navigation v3"
-    //val backStack = remember { mutableStateListOf<Any>(NavigationKey.Splash) }
 
     val backStack = rememberNavBackStack(Splash)
     NavDisplay(
@@ -34,7 +33,8 @@ fun NavHost() {
         entryDecorators = listOf(
             // Add the default decorators for managing scenes and saving state
             rememberSaveableStateHolderNavEntryDecorator(),
-            // Then add the view model store decorator
+
+            // Then add the view model store decorator - https://developer.android.com/guide/navigation/navigation-3/save-state#scoping-viewmodels
             rememberViewModelStoreNavEntryDecorator()
         ),
         entryProvider = entryProvider {
